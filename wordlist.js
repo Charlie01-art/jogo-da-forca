53a55fbab18c70f2cc1807eaf7725f06a3303666
Charlie <mirandawashigton191@gmail.com>
2024-12-02 21:25:45 -0300

    first commit

---
+++ wordlist.js
@@ -0,0 +1,167 @@
+
+// Check the source code for the questions
+const wordList = [
+    {
+      word: "guitarra",
+      hint: "Um instrumento musical com cordas.",
+    },
+    {
+      word: "oxigenio",
+      hint: "Um gás incolor e inodoro essencial à vida.",
+    },
+    
+    
+    {
+      word: "futebol",
+      hint: "Um esporte popular jogado com uma bola esférica.",
+    },
+    
+    {
+      word: "historia",
+      hint: "O estudo de eventos passados ​​e da civilização humana.",
+    },
+    {
+      word: "pizza",
+      hint: "Um prato saboroso composto por uma base redonda e achatada com coberturas.",
+    },
+    {
+      word: "jazz",
+      hint: "Um gênero musical caracterizado pela improvisação e síncope.",
+    },
+    {
+      word: "camera",
+      hint: "Um dispositivo usado para capturar e gravar imagens ou vídeos.",
+    },
+    {
+      word: "diamante",
+      hint: "Uma pedra preciosa conhecida por seu brilho e dureza.",
+    },
+    
+    {
+      word: "ciencia",
+      hint: "O estudo sistemático da estrutura e comportamento do mundo físico e natural.",
+    },
+    {
+      word: "bicicleta",
+      hint: "Um veículo movido a energia humana com duas rodas.",
+    },
+    {
+      word: "cafe",
+      hint: "Uma bebida popular brasileira, feito com grãos moídos.",
+    },
+    {
+      word: "danca",
+      hint: "Um movimento rítmico do corpo frequentemente executado com música.",
+    },
+    {
+      word: "galaxia",
+      hint: "Um vasto sistema de estrelas, gás e poeira mantidos juntos pela gravidade.",
+    },
+    
+  
+    {
+      word: "vulcao",
+      hint: "Um depósito magmático onde contém bolsões ou lagos subterrâneos de magma",
+    },
+    {
+      word: "romance",
+      hint: "Uma longa obra de ficção, normalmente com enredo e personagens complexos.",
+    },
+    
+    {
+      word: "bale",
+      hint: "Uma forma de dança clássica caracterizada por movimentos precisos e graciosos.",
+    },
+    {
+      word: "astronauta",
+      hint: "Uma pessoa treinada para viajar e trabalhar no espaço.",
+    },
+    {
+      word: "cachoeira",
+      hint: "Uma cascata de água caindo de uma altura.",
+    },
+    {
+      word: "universo",
+      hint: "Toda a matéria, espaço e tempo existentes como um todo.",
+    },
+    {
+      word: "piano",
+      hint: "Um instrumento musical tocado pressionando teclas que fazem os martelos baterem nas cordas.",
+    },
+    {
+      word: "ferias",
+      hint: "Um período de tempo dedicado ao prazer, descanso ou relaxamento.",
+    },
+  
+    {
+      word: "teatro",
+      hint: "Um edifício ou área externa onde são encenadas peças, filmes e apresentações.",
+    },
+    
+    {
+      word: "deserto",
+      hint: "Uma terra árida ou estéril com pouca ou nenhuma precipitação.",
+    },
+    {
+      word: "girassol",
+      hint: "Uma planta alta com uma grande flor amarela.",
+    },
+    {
+      word: "fantasia",
+      hint: "Um gênero de ficção imaginativa envolvendo elementos mágicos e sobrenaturais.",
+    },
+  
+    {
+      word: "brisa",
+      hint: "Um vento suave.",
+    },
+    {
+      word: "oasis",
+      hint: "Um local fértil no deserto onde se encontra água.",
+    },
+    
+    {
+      word: "safari",
+      hint: "Uma expedição ou viagem, normalmente para observar a vida selvagem no seu habitat natural.",
+    },
+    {
+      word: "planeta",
+      hint: "Um corpo celeste que orbita uma estrela e não produz luz própria.",
+    },
+    {
+      word: "rio",
+      hint: "Um grande fluxo natural de água fluindo em um canal para o mar, um lago ou outro riacho.",
+    },
+  
+    {
+      word: "misterios",
+      hint: "Difícil ou impossível de entender, explicar ou identificar.",
+    },
+    {
+      word: "enigma",
+      hint: "Algo misterioso, intrigante ou difícil de entender.",
+    },
+    {
+      word: "sussurrar",
+      hint: "Falar muito baixo ou baixo, muitas vezes de maneira secreta.",
+    },
+    {
+      word: "sombra",
+      hint: "Uma área ou forma escura produzida por um objeto ou pessoa bloqueando a luz.",
+    },
+    {
+      word: "segredo",
+      hint: "Algo mantido oculto ou desconhecido para os outros.",
+    },
+    {
+      word: "ilusão",
+      hint: "Uma falsa percepção ou crença; uma aparência ou impressão enganosa.",
+    },
+  
+    {
+      word: "vibrante",
+      hint: "Cheio de energia, brilho e vida.",
+    },
+    
+  ];
+  
